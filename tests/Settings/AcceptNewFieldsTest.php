afcee2236db0c91f628c742ac61c2a6400036b1c
Shobi P P <ppshobi@gmail.com>
2020-04-21 19:06:12 +0300

    Refactor Test class

--- tests/Settings/AcceptNewFieldsTest.php
+++ tests/Settings/AcceptNewFieldsTest.php
@@ -5,35 +5,29 @@ use Tests\TestCase;
 
 class AcceptNewFieldsTest extends TestCase
 {
-    private static $client;
-    private static $index;
+    private $index;
 
-    public static function setUpBeforeClass(): void
+    public function __construct()
     {
-        parent::setUpBeforeClass();
-        static::$client = new Client('http://localhost:7700', 'masterKey');
-        static::$client->deleteAllIndexes();
-        static::$index = static::$client->createIndex('uid');
-    }
+        parent::__construct();
 
-    public static function tearDownAfterClass(): void
-    {
-        parent::tearDownAfterClass();
-        static::$client->deleteAllIndexes();
+        $client = new Client('http://localhost:7700', 'masterKey');
+        $client->deleteAllIndexes();
+        $this->index = $client->createIndex('index');
     }
 
     public function testGetDefaultAcceptNewFields()
     {
-        $res = static::$index->getAcceptNewFields();
+        $res =$this->index->getAcceptNewFields();
         $this->assertTrue($res);
     }
 
     public function testUpdateAcceptNewFields()
     {
-        $res = static::$index->updateAcceptNewFields(false);
+        $res =$this->index->updateAcceptNewFields(false);
         $this->assertIsArray($res);
         $this->assertArrayHasKey('updateId', $res);
-        static::$index->waitForPendingUpdate($res['updateId']);
-        $this->assertFalse(static::$index->getAcceptNewFields());
+       $this->index->waitForPendingUpdate($res['updateId']);
+        $this->assertFalse($this->index->getAcceptNewFields());
     }
 }
