59aea521878975328a1139bdb8751174c7fe7d95
Shobi P P <ppshobi@gmail.com>
2020-04-22 13:38:01 +0300

    refactor and isolate distinct attribute test

--- tests/Settings/DistinctAttributeTest.php
+++ tests/Settings/DistinctAttributeTest.php
@@ -5,46 +5,44 @@ use Tests\TestCase;
 
 class DistinctAttributeTest extends TestCase
 {
-    private static $client;
-    private static $index;
+    private $client;
+    private $index;
 
-    public static function setUpBeforeClass(): void
+    public function __construct()
     {
-        parent::setUpBeforeClass();
-        static::$client = new Client('http://localhost:7700', 'masterKey');
-        static::$client->deleteAllIndexes();
-        static::$index = static::$client->createIndex('uid');
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        parent::tearDownAfterClass();
-        static::$client->deleteAllIndexes();
+        parent::__construct();
+        $this->client = new Client('http://localhost:7700', 'masterKey');
+        $this->client->deleteAllIndexes();
+        $this->index = $this->client->createIndex('index');
     }
 
     public function testGetDefaultDistinctAttribute()
     {
-        $res = static::$index->getDistinctAttribute();
-        $this->assertNull($res);
+        $response = $this->index->getDistinctAttribute();
+        $this->assertNull($response);
     }
 
     public function testUpdateDistinctAttribute()
     {
-        $new_da = 'description';
-        $res = static::$index->updateDistinctAttribute($new_da);
-        $this->assertIsArray($res);
-        $this->assertArrayHasKey('updateId', $res);
-        static::$index->waitForPendingUpdate($res['updateId']);
-        $da = static::$index->getDistinctAttribute();
-        $this->assertEquals($new_da, $da);
+        $distinctAttribute = 'description';
+        $promise = $this->index->updateDistinctAttribute($distinctAttribute);
+        $this->assertIsArray($promise);
+        $this->assertArrayHasKey('updateId', $promise);
+        $this->index->waitForPendingUpdate($promise['updateId']);
+        $this->assertEquals($distinctAttribute, $this->index->getDistinctAttribute());
     }
 
     public function testResetDistinctAttribute()
     {
-        $res = static::$index->resetDistinctAttribute();
-        $this->assertIsArray($res);
-        $this->assertArrayHasKey('updateId', $res);
-        static::$index->waitForPendingUpdate($res['updateId']);
-        $this->assertNull(static::$index->getDistinctAttribute());
+        $distinctAttribute = 'description';
+        $promise = $this->index->updateDistinctAttribute($distinctAttribute);
+        $this->index->waitForPendingUpdate($promise['updateId']);
+
+        $promise = $this->index->resetDistinctAttribute();
+
+        $this->assertIsArray($promise);
+        $this->assertArrayHasKey('updateId', $promise);
+        $this->index->waitForPendingUpdate($promise['updateId']);
+        $this->assertNull($this->index->getDistinctAttribute());
     }
 }
