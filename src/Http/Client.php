71ce5a1ce97a8570cee4a2e2a1bb80edfadbfd88
Shobi <ppshobi@gmail.com>
2020-06-19 01:23:36 +0300

    generate php doc for HTTP client class

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -45,6 +45,14 @@ class Client implements Http
     private $baseUrl;
 
 
+    /**
+     * Client constructor.
+     * @param $url
+     * @param null $apiKey
+     * @param ClientInterface|null $httpClient
+     * @param RequestFactoryInterface|null $requestFactory
+     * @param StreamFactoryInterface|null $streamFactory
+     */
     public function __construct($url, $apiKey = null, ClientInterface $httpClient = null, RequestFactoryInterface $requestFactory = null, StreamFactoryInterface $streamFactory = null)
     {
         $this->baseUrl = $url;
@@ -59,6 +67,13 @@ class Client implements Http
         $this->streamFactory = $streamFactory ?: Psr17FactoryDiscovery::findStreamFactory();
     }
 
+    /**
+     * @param $path
+     * @param array $query
+     * @return mixed
+     * @throws ClientExceptionInterface
+     * @throws HTTPRequestException
+     */
     public function get($path, $query = [])
     {
         $request    = $this->requestFactory->createRequest(
@@ -69,6 +84,14 @@ class Client implements Http
         return $this->execute($request);
     }
 
+    /**
+     * @param string $path
+     * @param null $body
+     * @param array $query
+     * @return mixed
+     * @throws ClientExceptionInterface
+     * @throws HTTPRequestException
+     */
     public function post($path, $body = null, $query = [])
     {
         $request = $this->requestFactory->createRequest(
@@ -89,6 +112,14 @@ class Client implements Http
         return $this->execute($request);
     }
 
+    /**
+     * @param string $path
+     * @param null $body
+     * @param array $query
+     * @return mixed
+     * @throws ClientExceptionInterface
+     * @throws HTTPRequestException
+     */
     public function patch($path, $body = null, $query = [])
     {
         $request = $this->requestFactory->createRequest(
@@ -99,6 +130,13 @@ class Client implements Http
         return $this->execute($request);
     }
 
+    /**
+     * @param $path
+     * @param array $query
+     * @return mixed
+     * @throws ClientExceptionInterface
+     * @throws HTTPRequestException
+     */
     public function delete($path, $query = [])
     {
         $request = $this->requestFactory->createRequest(
@@ -109,6 +147,12 @@ class Client implements Http
         return $this->execute($request);
     }
 
+    /**
+     * @param RequestInterface $request
+     * @return mixed
+     * @throws ClientExceptionInterface
+     * @throws HTTPRequestException
+     */
     private function execute(RequestInterface $request)
     {
         foreach ($this->headers as $header => $value) {
@@ -118,6 +162,10 @@ class Client implements Http
         return $this->parseResponse($this->http->sendRequest($request));
     }
 
+    /**
+     * @param array $queryParams
+     * @return string
+     */
     private function buildQueryString(array $queryParams = []): string
     {
         return $queryParams ? '?' . http_build_query($queryParams) : '';
