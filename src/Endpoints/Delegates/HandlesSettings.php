b13ed6f6e6f6c3929f083e46e367593ce069a0fd
mishu <mishu@xtelecom.ro>
2021-09-08 10:07:46 +0300

    reversed formatting

--- src/Endpoints/Delegates/HandlesSettings.php
+++ src/Endpoints/Delegates/HandlesSettings.php
@@ -15,118 +15,118 @@ trait HandlesSettings
 
     public function getRankingRules(): array
     {
-        return $this->http->get(self::PATH . '/' . $this->uid . '/settings/ranking-rules');
+        return $this->http->get(self::PATH.'/'.$this->uid.'/settings/ranking-rules');
     }
 
     public function updateRankingRules(array $rankingRules): array
     {
-        return $this->http->post(self::PATH . '/' . $this->uid . '/settings/ranking-rules', $rankingRules);
+        return $this->http->post(self::PATH.'/'.$this->uid.'/settings/ranking-rules', $rankingRules);
     }
 
     public function resetRankingRules(): array
     {
-        return $this->http->delete(self::PATH . '/' . $this->uid . '/settings/ranking-rules');
+        return $this->http->delete(self::PATH.'/'.$this->uid.'/settings/ranking-rules');
     }
 
     // Settings - Distinct attribute
 
     public function getDistinctAttribute()
     {
-        return $this->http->get(self::PATH . '/' . $this->uid . '/settings/distinct-attribute');
+        return $this->http->get(self::PATH.'/'.$this->uid.'/settings/distinct-attribute');
     }
 
     public function updateDistinctAttribute(string $distinctAttribute): array
     {
-        return $this->http->post(self::PATH . '/' . $this->uid . '/settings/distinct-attribute', $distinctAttribute);
+        return $this->http->post(self::PATH.'/'.$this->uid.'/settings/distinct-attribute', $distinctAttribute);
     }
 
     public function resetDistinctAttribute(): array
     {
-        return $this->http->delete(self::PATH . '/' . $this->uid . '/settings/distinct-attribute');
+        return $this->http->delete(self::PATH.'/'.$this->uid.'/settings/distinct-attribute');
     }
 
     // Settings - Searchable attributes
 
     public function getSearchableAttributes(): array
     {
-        return $this->http->get(self::PATH . '/' . $this->uid . '/settings/searchable-attributes');
+        return $this->http->get(self::PATH.'/'.$this->uid.'/settings/searchable-attributes');
     }
 
     public function updateSearchableAttributes(array $searchableAttributes): array
     {
-        return $this->http->post(self::PATH . '/' . $this->uid . '/settings/searchable-attributes', $searchableAttributes);
+        return $this->http->post(self::PATH.'/'.$this->uid.'/settings/searchable-attributes', $searchableAttributes);
     }
 
     public function resetSearchableAttributes(): array
     {
-        return $this->http->delete(self::PATH . '/' . $this->uid . '/settings/searchable-attributes');
+        return $this->http->delete(self::PATH.'/'.$this->uid.'/settings/searchable-attributes');
     }
 
     // Settings - Displayed attributes
 
     public function getDisplayedAttributes(): array
     {
-        return $this->http->get(self::PATH . '/' . $this->uid . '/settings/displayed-attributes');
+        return $this->http->get(self::PATH.'/'.$this->uid.'/settings/displayed-attributes');
     }
 
     public function updateDisplayedAttributes(array $displayedAttributes): array
     {
-        return $this->http->post(self::PATH . '/' . $this->uid . '/settings/displayed-attributes', $displayedAttributes);
+        return $this->http->post(self::PATH.'/'.$this->uid.'/settings/displayed-attributes', $displayedAttributes);
     }
 
     public function resetDisplayedAttributes(): array
     {
-        return $this->http->delete(self::PATH . '/' . $this->uid . '/settings/displayed-attributes');
+        return $this->http->delete(self::PATH.'/'.$this->uid.'/settings/displayed-attributes');
     }
 
     // Settings - Stop-words
 
     public function getStopWords(): array
     {
-        return $this->http->get(self::PATH . '/' . $this->uid . '/settings/stop-words');
+        return $this->http->get(self::PATH.'/'.$this->uid.'/settings/stop-words');
     }
 
     public function updateStopWords(array $stopWords): array
     {
-        return $this->http->post(self::PATH . '/' . $this->uid . '/settings/stop-words', $stopWords);
+        return $this->http->post(self::PATH.'/'.$this->uid.'/settings/stop-words', $stopWords);
     }
 
     public function resetStopWords(): array
     {
-        return $this->http->delete(self::PATH . '/' . $this->uid . '/settings/stop-words');
+        return $this->http->delete(self::PATH.'/'.$this->uid.'/settings/stop-words');
     }
 
     // Settings - Synonyms
 
     public function getSynonyms(): array
     {
-        return $this->http->get(self::PATH . '/' . $this->uid . '/settings/synonyms');
+        return $this->http->get(self::PATH.'/'.$this->uid.'/settings/synonyms');
     }
 
     public function updateSynonyms(array $synonyms): array
     {
-        return $this->http->post(self::PATH . '/' . $this->uid . '/settings/synonyms', $synonyms);
+        return $this->http->post(self::PATH.'/'.$this->uid.'/settings/synonyms', $synonyms);
     }
 
     public function resetSynonyms(): array
     {
-        return $this->http->delete(self::PATH . '/' . $this->uid . '/settings/synonyms');
+        return $this->http->delete(self::PATH.'/'.$this->uid.'/settings/synonyms');
     }
 
     // Settings - Filterable Attributes
 
     public function getFilterableAttributes(): array
     {
-        return $this->http->get(self::PATH . '/' . $this->uid . '/settings/filterable-attributes');
+        return $this->http->get(self::PATH.'/'.$this->uid.'/settings/filterable-attributes');
     }
 
     public function updateFilterableAttributes(array $filterableAttributes): array
     {
-        return $this->http->post(self::PATH . '/' . $this->uid . '/settings/filterable-attributes', $filterableAttributes);
+        return $this->http->post(self::PATH.'/'.$this->uid.'/settings/filterable-attributes', $filterableAttributes);
     }
 
     public function resetFilterableAttributes(): array
     {
-        return $this->http->delete(self::PATH . '/' . $this->uid . '/settings/filterable-attributes');
+        return $this->http->delete(self::PATH.'/'.$this->uid.'/settings/filterable-attributes');
     }
 }
