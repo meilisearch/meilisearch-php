57b0560c83f30d5199e7e710cf3070eb1c5e6653
alallema <amelie@meilisearch.com>
2022-04-21 14:54:17 +0200

    Ensure nested field support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,6 +20,16 @@ abstract class TestCase extends BaseTestCase
         ['id' => 42, 'title' => 'The Hitchhiker\'s Guide to the Galaxy'],
     ];
 
+    protected const NESTED_DOCUMENTS = [
+        ['id' => 1, 'title' => 'Pride and Prejudice', 'info' => ['comment' => 'A great book',   'reviewNb' => 50]],
+        ['id' => 2, 'title' => 'Le Petit Prince', 'info' => ['comment' => 'A french book',   'reviewNb' => 600]],
+        ['id' => 3, 'title' => 'Le Rouge et le Noir', 'info' => ['comment' => 'Another french book','reviewNb' => 700]],
+        ['id' => 4, 'title' => 'Alice In Wonderland', 'comment' => 'A weird book', 'info' => ['comment' => 'A weird book', 'reviewNb' => 800]],
+        ['id' => 5, 'title' => 'The Hobbit', 'info' => ['comment' => 'An awesome book', 'reviewNb' => 900]],
+        ['id' => 6, 'title' => 'Harry Potter and the Half-Blood Prince', 'info' => ['comment' => 'The best book', 'reviewNb' => 1000]],
+        ['id' => 7, 'title' => "The Hitchhiker's Guide to the Galaxy"],
+    ];
+
     protected const INFO_KEY = [
         'actions' => ['search'],
         'indexes' => ['index'],
