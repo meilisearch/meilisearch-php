299d9b015a5eb2f9e4fdfed2e70dffe2390ae373
Bruno Casali <brunoocasali@gmail.com>
2022-07-02 11:25:27 -0300

    Add TasksQuery and TasksResults to handle tasks

---
+++ tests/Contracts/TasksQueryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Contracts;
+
+use MeiliSearch\Contracts\TasksQuery;
+use PHPUnit\Framework\TestCase;
+
+class TasksQueryTest extends TestCase
+{
+    public function testSetTypes(): void
+    {
+        $data = (new TasksQuery())->setTypes(['abc', 'xyz']);
+
+        $this->assertEquals($data->toArray(), ['type' => 'abc,xyz']);
+    }
+
+    public function testSetNext(): void
+    {
+        $data = (new TasksQuery())->setNext(99);
+
+        $this->assertEquals($data->toArray(), ['next' => 99]);
+    }
+
+    public function testToArrayWithSetLimit(): void
+    {
+        $data = (new TasksQuery())->setLimit(10);
+
+        $this->assertEquals($data->toArray(), ['limit' => 10]);
+    }
+
+    public function testToArrayWithDifferentSets(): void
+    {
+        $data = (new TasksQuery())->setFrom(10)->setLimit(9)->setNext(99)->setStatus(['enqueued']);
+
+        $this->assertEquals($data->toArray(), [
+            'limit' => 9, 'next' => 99, 'from' => 10, 'status' => 'enqueued',
+        ]);
+    }
+}
