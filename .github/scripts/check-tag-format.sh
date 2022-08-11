c43bc284bcf9beedbbb92141c4dc03969bf23ea2
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-08-11 11:14:30 +0200

    Add CI and doc to release a beta version
    Remove check on version in composer file
    
    WIP: Test beta CI (#367)
    
    * Test beta CI
    
    * Add github checkout action

---
+++ .github/scripts/check-tag-format.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+meilisearch_php_version=$(grep 'public const VERSION =' src/MeiliSearch.php | cut -d ' ' -f 9 | tr -d "'" | tr -d ";")
+
+# Works with the format vX.X.X-xxx-beta.X
+# none or multiple -xxx are valid
+#
+# Examples of correct format:
+# v0.1.0-beta.0
+# v0.1.0-xxx-beta.0
+# v0.1.0-xxx-xxx-beta.0
+echo "$meilisearch_php_version" | grep -E "[0-9]*\.[0-9]*\.[0-9]*-([a-z]*-)*beta\.[0-9]*$"
+if [ $? != 0 ]; then
+    echo "Error: Your beta tag in src/Meilisearch.php: $meilisearch_php_version is wrongly formatted."
+    echo 'Please refer to the contributing guide for help.'
+    exit 1
+fi
+
+exit 0
