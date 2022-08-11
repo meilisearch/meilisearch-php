5fde5faf4d0d6c260ec88ecb6810c96e5e2fe09e
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-08-11 12:07:53 +0200

    Add specific types of beta

--- src/MeiliSearch.php
+++ src/MeiliSearch.php
@@ -6,7 +6,7 @@ namespace MeiliSearch;
 
 class MeiliSearch
 {
-    public const VERSION = '0.24.1-pagination-beta.0';
+    public const VERSION = '0.25.0-meilisearch-pagination-beta.0';
 
     public static function qualifiedVersion()
     {
