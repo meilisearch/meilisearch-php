fa17e1930dddd545e8ad8af3c6c7ea31cd4ed992
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-08-11 12:25:42 +0200

    Update beta docker version with latest addition of meilisearch- in front of prototype naming

--- .github/scripts/beta-docker-version.sh
+++ .github/scripts/beta-docker-version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 meilisearch_php_version=$(grep 'public const VERSION =' ./src/MeiliSearch.php | cut -d ' ' -f 9 | tr -d "'" | tr -d ";")
-beta_feature=$(echo $meilisearch_php_version | sed -r 's/[0-9]+.[0-9]+.[0-9]+-//')
+beta_feature=$(echo $meilisearch_php_version | sed -r 's/[0-9]+.[0-9]+.[0-9]+-meilisearch-//')
 beta_feature=$(echo $beta_feature | sed -r 's/-beta\.[0-9]*$//')
 
 docker_image=$(curl https://hub.docker.com/v2/repositories/getmeili/meilisearch/tags | jq | grep "$beta_feature" | head -1)
