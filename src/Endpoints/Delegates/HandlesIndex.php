0e9c5878fab58b20496fa1633daace1825f2417b
Bruno Casali <brunoocasali@gmail.com>
2023-01-31 22:54:56 -0300

    Move src/Delegates/* to src/Endpoints/Delegates/*

--- src/Endpoints/Delegates/HandlesIndex.php
+++ src/Endpoints/Delegates/HandlesIndex.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Meilisearch\Delegates;
+namespace Meilisearch\Endpoints\Delegates;
 
 use Meilisearch\Contracts\IndexesQuery;
 use Meilisearch\Contracts\IndexesResults;
