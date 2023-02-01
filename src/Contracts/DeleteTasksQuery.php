e8522f8e57a8f9e6a2126092671e23dfce647b3c
Bruno Casali <brunoocasali@gmail.com>
2023-01-31 22:29:06 -0300

    Correct DeleteTasksQuery allowed attributes

--- src/Contracts/DeleteTasksQuery.php
+++ src/Contracts/DeleteTasksQuery.php
@@ -9,4 +9,23 @@ use Meilisearch\Delegates\TasksQueryTrait;
 class DeleteTasksQuery
 {
     use TasksQueryTrait;
+
+    private array $canceledBy;
+
+    public function setCanceledBy(array $canceledBy)
+    {
+        $this->canceledBy = $canceledBy;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return array_filter(
+            array_merge(
+                $this->baseArray(),
+                ['canceledBy' => $this->formatArray($this->canceledBy ?? null)]
+            ), function ($item) { return null != $item || is_numeric($item); }
+        );
+    }
 }
