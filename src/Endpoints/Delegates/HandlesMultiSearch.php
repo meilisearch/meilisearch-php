25a5e5deef68f781debae05bd826803d9a690fde
Bruno Casali <brunoocasali@gmail.com>
2023-03-28 09:48:12 -0300

    Add `multiSearch` (#481)
    Add multiSearch method on client

---
+++ src/Endpoints/Delegates/HandlesMultiSearch.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Meilisearch\Endpoints\Delegates;
+
+trait HandlesMultiSearch
+{
+    /**
+     * @param list<\Meilisearch\Contracts\SearchQuery> $queries
+     */
+    public function multiSearch(array $queries = [])
+    {
+        $body = [];
+
+        foreach ($queries as $query) {
+            $body[] = $query->toArray();
+        }
+
+        return $this->http->post('/multi-search', ['queries' => $body]);
+    }
+}
--- tests/Contracts/IndexesQueryTest.php
+++ tests/Contracts/IndexesQueryTest.php
@@ -13,27 +13,27 @@ class IndexesQueryTest extends TestCase
     {
         $data = (new IndexesQuery())->setLimit(10)->setOffset(18);
 
-        $this->assertEquals($data->toArray(), ['limit' => 10, 'offset' => 18]);
+        $this->assertEquals(['limit' => 10, 'offset' => 18], $data->toArray());
     }
 
     public function testToArrayWithSetOffset(): void
     {
         $data = (new IndexesQuery())->setOffset(5);
 
-        $this->assertEquals($data->toArray(), ['offset' => 5]);
+        $this->assertEquals(['offset' => 5], $data->toArray());
     }
 
     public function testToArrayWithoutSet(): void
     {
         $data = new IndexesQuery();
 
-        $this->assertEquals($data->toArray(), []);
+        $this->assertEmpty($data->toArray());
     }
 
     public function testToArrayWithZeros(): void
     {
         $data = (new IndexesQuery())->setLimit(0)->setOffset(0);
 
-        $this->assertEquals($data->toArray(), ['limit' => 0, 'offset' => 0]);
+        $this->assertEquals(['limit' => 0, 'offset' => 0], $data->toArray());
     }
 }
