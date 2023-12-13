a69c60a4a423d84d66a7057b968af10d216f06b3
Tomas <norkunas.tom@gmail.com>
2023-12-13 09:21:24 +0200

    Declare `@throws` in HandlesTasks delegate

--- src/Endpoints/Delegates/HandlesTasks.php
+++ src/Endpoints/Delegates/HandlesTasks.php
@@ -9,6 +9,7 @@ use Meilisearch\Contracts\DeleteTasksQuery;
 use Meilisearch\Contracts\TasksQuery;
 use Meilisearch\Contracts\TasksResults;
 use Meilisearch\Endpoints\Tasks;
+use Meilisearch\Exceptions\TimeOutException;
 
 trait HandlesTasks
 {
@@ -38,11 +39,17 @@ trait HandlesTasks
         return $this->tasks->cancelTasks($options);
     }
 
+    /**
+     * @throws TimeOutException
+     */
     public function waitForTask($uid, int $timeoutInMs = 5000, int $intervalInMs = 50): array
     {
         return $this->tasks->waitTask($uid, $timeoutInMs, $intervalInMs);
     }
 
+    /**
+     * @throws TimeOutException
+     */
     public function waitForTasks($uids, int $timeoutInMs = 5000, int $intervalInMs = 50): array
     {
         return $this->tasks->waitTasks($uids, $timeoutInMs, $intervalInMs);
