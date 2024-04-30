67ec4b32f46f6a5d7b55257720ff20f78dbdcd2a
Bruno Casali <brunoocasali@gmail.com>
2024-04-30 10:23:28 -0300

    Add searchCutoffMs methods

---
+++ tests/Settings/SearchCutoffMsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Settings;
+
+use Meilisearch\Endpoints\Indexes;
+use Tests\TestCase;
+
+final class SearchCutoffMsTest extends TestCase
+{
+    private Indexes $index;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->index = $this->createEmptyIndex($this->safeIndexName());
+    }
+
+    public function testGetDefaultSearchCutoffMs(): void
+    {
+        $default = $this->index->getSearchCutoffMs();
+
+        self::assertNull($default);
+    }
+
+    public function testUpdateSearchCutoffMs(): void
+    {
+        $promise = $this->index->updateSearchCutoffMs(50);
+        $this->assertIsValidPromise($promise);
+        $this->index->waitForTask($promise['taskUid']);
+
+        self::assertSame(50, $this->index->getSearchCutoffMs());
+    }
+
+    public function testResetSearchCutoffMs(): void
+    {
+        $promise = $this->index->updateSearchCutoffMs(50);
+        $this->assertIsValidPromise($promise);
+        $this->index->waitForTask($promise['taskUid']);
+
+        $promise = $this->index->resetSearchCutoffMs();
+
+        $this->assertIsValidPromise($promise);
+        $this->index->waitForTask($promise['taskUid']);
+
+        self::assertNull($this->index->getSearchCutoffMs());
+    }
+}
