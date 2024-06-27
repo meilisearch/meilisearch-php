c2885bfa5deddc795674ed645e9b2cfa4a077e21
Tomas <norkunas.tom@gmail.com>
2024-06-27 06:44:22 +0300

    Enable cs fixer parallel config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,22 +3,19 @@
 declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__.DIRECTORY_SEPARATOR.'src')
-    ->in(__DIR__.DIRECTORY_SEPARATOR.'tests')
-    ->append(['.php_cs.dist']);
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
+    ->append([__FILE__]);
 
-$rules = [
-    '@Symfony' => true,
-    'declare_strict_types' => true,
-    'void_return' => true,
-    'native_function_invocation' => ['include' => ['@compiler_optimized'], 'scope' => 'namespaced'],
-    'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
-    'php_unit_strict' => true,
-];
-
-$config = new PhpCsFixer\Config();
-
-return $config
-    ->setRules($rules)
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRules([
+        '@Symfony' => true,
+        'declare_strict_types' => true,
+        'void_return' => true,
+        'native_function_invocation' => ['include' => ['@compiler_optimized'], 'scope' => 'namespaced'],
+        'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
+        'php_unit_strict' => true,
+    ])
     ->setRiskyAllowed(true)
     ->setFinder($finder);
