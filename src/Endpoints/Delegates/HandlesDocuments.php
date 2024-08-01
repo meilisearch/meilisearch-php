3c237bb83dcd58f59e6945cc7fe52d68c4b7d18c
Tamo <irevoire@protonmail.ch>
2024-08-01 13:32:41 +0200

    Implements the update documents by function (#664)
    * Edit documents by function
    
    * Apply suggestions from code review
    
    Co-authored-by: Tomas Norkūnas <norkunas.tom@gmail.com>
    
    * make linter happy
    
    * make phpstan happy
    
    * Update src/Endpoints/Delegates/HandlesDocuments.php
    
    Co-authored-by: Tomas Norkūnas <norkunas.tom@gmail.com>
    
    * Update src/Endpoints/Delegates/HandlesDocuments.php
    
    Co-authored-by: Tomas Norkūnas <norkunas.tom@gmail.com>
    
    * do not use the splat operator on arraymap
    
    * make linter happy
    
    ---------
    
    Co-authored-by: Tomas Norkūnas <norkunas.tom@gmail.com>
    Co-authored-by: Clémentine <clementine@meilisearch.com>

--- src/Endpoints/Delegates/HandlesDocuments.php
+++ src/Endpoints/Delegates/HandlesDocuments.php
@@ -139,6 +139,21 @@ trait HandlesDocuments
         return $promises;
     }
 
+    /**
+     * This is an EXPERIMENTAL feature, which may break without a major version.
+     * It's available after Meilisearch v1.10.
+     *
+     * More info about the feature: https://github.com/orgs/meilisearch/discussions/762
+     * More info about experimental features in general: https://www.meilisearch.com/docs/reference/api/experimental-features
+     *
+     * @param non-empty-string                                                                                       $function
+     * @param array{filter?: non-empty-string|list<non-empty-string>|null, context?: array<non-empty-string, mixed>} $options
+     */
+    public function updateDocumentsByFunction(string $function, array $options = [])
+    {
+        return $this->http->post(self::PATH.'/'.$this->uid.'/documents/edit', array_merge(['function' => $function], $options));
+    }
+
     public function deleteAllDocuments(): array
     {
         return $this->http->delete(self::PATH.'/'.$this->uid.'/documents');
