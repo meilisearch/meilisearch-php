1fb211f182df434c50eea9b46d9f5aa102a92759
Tamo <irevoire@protonmail.ch>
2024-08-01 10:19:35 +0200

    Update src/Endpoints/Delegates/HandlesDocuments.php
    Co-authored-by: Tomas Norkūnas <norkunas.tom@gmail.com>

--- src/Endpoints/Delegates/HandlesDocuments.php
+++ src/Endpoints/Delegates/HandlesDocuments.php
@@ -147,9 +147,9 @@ trait HandlesDocuments
      * More info about experimental features in general: https://www.meilisearch.com/docs/reference/api/experimental-features
      *
      * @param non-empty-string $function
-     * @param array{filter: non-empty-string|list<non-empty-string>|null, context: array<non-empty-string, mixed>}|null $options
+     * @param array{filter: non-empty-string|list<non-empty-string>|null, context: array<non-empty-string, mixed>} $options
      */
-    public function updateDocumentsByFunction(string $function, ?array $options)
+    public function updateDocumentsByFunction(string $function, array $options = [])
     {
         return $this->http->post(self::PATH.'/'.$this->uid.'/documents/edit', ['function' => $function, ...$options]);
     }
