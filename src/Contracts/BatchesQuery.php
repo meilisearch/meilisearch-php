3783e043233c4e972917f6737d575f62d843210a
Strift <lau.cazanove@gmail.com>
2024-12-04 19:13:30 +0800

    Add tests for fetching batches

---
+++ src/Contracts/BatchesQuery.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Meilisearch\Contracts;
+
+use Meilisearch\Endpoints\Delegates\TasksQueryTrait;
+
+class BatchesQuery
+{
+  use TasksQueryTrait;
+
+  private ?int $from = null;
+
+  /**
+   * @var non-negative-int|null
+   */
+  private ?int $limit = null;
+
+  /**
+   * @var non-empty-list<int>|null
+   */
+  private ?array $canceledBy = null;
+
+  /**
+   * @var bool|null
+   */
+  private ?bool $reverse = null;
+
+  /**
+   * @return $this
+   */
+  public function setFrom(int $from): self
+  {
+    $this->from = $from;
+
+    return $this;
+  }
+
+  /**
+   * @param non-empty-list<int> $canceledBy
+   *
+   * @return $this
+   */
+  public function setCanceledBy(array $canceledBy): self
+  {
+    $this->canceledBy = $canceledBy;
+
+    return $this;
+  }
+
+  /**
+   * @return $this
+   */
+  public function setLimit(int $limit): self
+  {
+    $this->limit = $limit;
+
+    return $this;
+  }
+
+  /**
+   * @return $this
+   */
+  public function setReverse(bool $reverse): self
+  {
+    $this->reverse = $reverse;
+
+    return $this;
+  }
+
+  public function toArray(): array
+  {
+    return array_filter(
+      array_merge(
+        $this->baseArray(),
+        [
+          'from' => $this->from,
+          'limit' => $this->limit,
+          'reverse' => (null !== $this->reverse ? ($this->reverse ? 'true' : 'false') : null),
+        ]
+      ),
+      static function ($item) {
+        return null !== $item;
+      }
+    );
+  }
+}
--- tests/Endpoints/BatchesTest.php
+++ tests/Endpoints/BatchesTest.php
@@ -4,26 +4,57 @@ declare(strict_types=1);
 
 namespace Tests\Endpoints;
 
+use Meilisearch\Contracts\TasksQuery;
 use Meilisearch\Contracts\BatchesQuery;
 use Meilisearch\Endpoints\Indexes;
 use Meilisearch\Exceptions\ApiException;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertNotNull;
+
 final class BatchesTest extends TestCase
 {
-  private Indexes $index;
   private string $indexName;
 
   protected function setUp(): void
   {
     parent::setUp();
     $this->indexName = $this->safeIndexName();
-    $this->index = $this->createEmptyIndex($this->indexName);
+    $this->createEmptyIndex($this->indexName);
   }
 
   public function testGetAllBatches(): void
   {
     $response = $this->client->getBatches();
     self::assertIsArray($response->getResults());
+  }
+
+  public function testGetAllBatchesWithIndexUidFilters(): void
+  {
+    $response = $this->client->getBatches((new BatchesQuery())->setIndexUids([$this->indexName]));
+    foreach ($response->getResults() as $result) {
+      self::assertArrayHasKey($this->indexName, $result['stats']['indexUids']);
+    }
+  }
+
+  public function testGetAllBatchesWithTasksFilters(): void
+  {
+    $tasks = $this->client->getTasks(new TasksQuery())->getResults();
+    $response = $this->client->getBatches((new BatchesQuery())->setUids([$tasks[0]['uid']]));
+    self::assertNotNull($response->getResults());
+  }
+
+  public function testGetAllBatchesInReverseOrder(): void
+  {
+    $startDate = new \DateTimeImmutable('now');
+
+    $batches = $this->client->getBatches((new BatchesQuery())
+        ->setAfterEnqueuedAt($startDate)
+    );
+    $reversedBatches = $this->client->getBatches((new BatchesQuery())
+        ->setAfterEnqueuedAt($startDate)
+        ->setReverse(true)
+    );
+    self::assertEquals($batches->getResults(), array_reverse($reversedBatches->getResults()));
   }
 }
