97ef752e177d8f7a1775d1fa5ee2e730c9937d73
Strift <lau.cazanove@gmail.com>
2025-03-26 14:47:17 +0800

    Add tesdt to check batches stats have progressTrace

--- tests/Endpoints/BatchesTest.php
+++ tests/Endpoints/BatchesTest.php
@@ -65,10 +65,10 @@ final class BatchesTest extends TestCase
         self::assertArrayHasKey('status', $response['stats']);
         self::assertArrayHasKey('types', $response['stats']);
         self::assertArrayHasKey('indexUids', $response['stats']);
+        self::assertArrayHasKey('progressTrace', $response['stats']);
         self::assertArrayHasKey('duration', $response);
         self::assertArrayHasKey('startedAt', $response);
         self::assertArrayHasKey('finishedAt', $response);
         self::assertArrayHasKey('progress', $response);
-        self::assertArrayHasKey('stats', $response);
     }
 }
