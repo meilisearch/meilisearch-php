b1b31a882df92de7cbeb740b88e90e065371b081
Tomas <norkunas.tom@gmail.com>
2025-04-09 14:44:29 +0300

    Add missing types

--- src/Endpoints/Delegates/HandlesIndex.php
+++ src/Endpoints/Delegates/HandlesIndex.php
@@ -17,31 +17,49 @@ trait HandlesIndex
         return $this->index->all($options ?? null);
     }
 
+    /**
+     * @param non-empty-string $uid
+     */
     public function getRawIndex(string $uid): array
     {
         return $this->index($uid)->fetchRawInfo();
     }
 
+    /**
+     * @param non-empty-string $uid
+     */
     public function index(string $uid): Indexes
     {
         return new Indexes($this->http, $uid);
     }
 
+    /**
+     * @param non-empty-string $uid
+     */
     public function getIndex(string $uid): Indexes
     {
         return $this->index($uid)->fetchInfo();
     }
 
+    /**
+     * @param non-empty-string $uid
+     */
     public function deleteIndex(string $uid): array
     {
         return $this->index($uid)->delete();
     }
 
+    /**
+     * @param non-empty-string $uid
+     */
     public function createIndex(string $uid, array $options = []): array
     {
         return $this->index->create($uid, $options);
     }
 
+    /**
+     * @param non-empty-string $uid
+     */
     public function updateIndex(string $uid, array $options = []): array
     {
         return $this->index($uid)->update($options);
