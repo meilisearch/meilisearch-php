5036e80509a1378e0b3f743d36035a962718dfc6
Tomas Norkūnas <norkunas.tom@gmail.com>
2025-05-15 07:33:41 +0300

    Fix TaskDeletionDetails to accept `deletedTasks`

--- src/Contracts/TaskDetails/TaskDeletionDetails.php
+++ src/Contracts/TaskDetails/TaskDeletionDetails.php
@@ -9,7 +9,7 @@ use Meilisearch\Contracts\TaskDetails;
 /**
  * @implements TaskDetails<array{
  *     matchedTasks: non-negative-int|null,
- *     canceledTasks: non-negative-int|null,
+ *     deletedTasks: non-negative-int|null,
  *     originalFilter: string|null
  * }>
  */
@@ -17,12 +17,12 @@ final class TaskDeletionDetails implements TaskDetails
 {
     /**
      * @param non-negative-int|null $matchedTasks   The number of matched tasks. If the API key used for the request doesn’t have access to an index, tasks relating to that index will not be included in matchedTasks.
-     * @param non-negative-int|null $canceledTasks  The number of tasks successfully canceled. If the task cancellation fails, this will be 0. null when the task status is enqueued or processing.
-     * @param string|null           $originalFilter the filter used in the delete task request
+     * @param non-negative-int|null $deletedTasks   The number of tasks successfully deleted. If the task deletion fails, this will be 0. null when the task status is enqueued or processing.
+     * @param string|null           $originalFilter The filter used in the delete task request.
      */
     public function __construct(
         public readonly ?int $matchedTasks,
-        public readonly ?int $canceledTasks,
+        public readonly ?int $deletedTasks,
         public readonly ?string $originalFilter,
     ) {
     }
@@ -31,7 +31,7 @@ final class TaskDeletionDetails implements TaskDetails
     {
         return new self(
             $data['matchedTasks'],
-            $data['canceledTasks'],
+            $data['deletedTasks'],
             $data['originalFilter'],
         );
     }
