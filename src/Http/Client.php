f888fd70ce94578ad201894cc942e929be1f854d
Tomas Norkūnas <norkunas.tom@gmail.com>
2025-06-04 07:29:56 +0300

    Avoid count

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -159,7 +159,7 @@ class Client implements Http
 
     private function buildQueryString(array $queryParams = []): string
     {
-        return \count($queryParams) > 0 ? '?'.http_build_query($queryParams) : '';
+        return $queryParams !== [] ? '?'.http_build_query($queryParams) : '';
     }
 
     /**
