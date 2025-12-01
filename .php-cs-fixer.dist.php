48bd5c638b2b8d1ae6dd852a94ca533debd04b97
Tomas Norkūnas <norkunas.tom@gmail.com>
2025-12-01 07:27:50 +0200

    Fix PHP-CS-Fixer deprecation

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,6 +9,7 @@ $finder = PhpCsFixer\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setUnsupportedPhpVersionAllowed(true)
     ->setRules([
         '@Symfony' => true,
         'declare_strict_types' => true,
