0ffd76aa4c3771ee78fa16773109516394dbc0dc
Strift <lau.cazanove@gmail.com>
2026-03-11 15:51:44 +0800

    lint

--- tests/Endpoints/NetworksTest.php
+++ tests/Endpoints/NetworksTest.php
@@ -35,7 +35,7 @@ final class NetworksTest extends TestCase
         ];
 
         $this->expectException(\InvalidArgumentException::class);
-        /** @phpstan-ignore-next-line intentional invalid input to test validation */
+        /* @phpstan-ignore-next-line intentional invalid input to test validation */
         $this->client->initializeNetwork($options);
     }
 
